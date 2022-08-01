bba271aac29953fde959f91fd8e7e98e991843a8
Sam Wilcox <swilcox21@gmail.com>
2022-08-01 16:12:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>BABY SHOWER DRINK LIST</h1>
+        <br/>
+        <div>MARGARITA</div>
+        <br/>
+        <div>OLD FASHIONED</div>
+        <br/>
+        <div>MAI TAI</div>
+        <br/>
+        <div>MOJITO</div>
+        <br/>
+        <div>LEMON BASIL MARTINI</div>
+    </div>
+  );
+}
+
+export default App;
