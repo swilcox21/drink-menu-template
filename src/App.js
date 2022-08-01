458173aa3dbefbc744e42daf583a7822258090ae
Sam Wilcox <swilcox21@gmail.com>
2022-08-01 18:43:46 -0400

    adding image to repo

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>BABY SHOWER DRINK LIST</h1>
-        <br/>
-        <div>MARGARITA</div>
-        <br/>
-        <div>OLD FASHIONED</div>
-        <br/>
-        <div>MAI TAI</div>
-        <br/>
-        <div>MOJITO (please don't order)</div>
-        <br/>
-        <div>LEMON BASIL MARTINI</div>
-    </div>
+      <img className="drinkList" src="https://secure.img1-fg.wfcdn.com/im/55143880/resize-h445%5Ecompr-r85/1444/14441004/Menu+Wall+Mounted+Chalkboard.jpg" alt=""/>
+          <div>MARGARITA</div>
+          <br/>
+          <div>OLD FASHIONED</div>
+          <br/>
+          <div>MAI TAI</div>
+          <br/>
+          <div>MOJITO (please don't order)</div>
+          <br/>
+          <div>LEMON BASIL MARTINI</div>
+
+      </div>
   );
 }
 
