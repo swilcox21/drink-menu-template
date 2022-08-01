0a38212dfaf8f8e09ce89f19883fb6b4543a1b86
Sam Wilcox <swilcox21@gmail.com>
2022-08-01 19:22:06 -0400

    initial framework in place, just need actual content

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <img className="drinkList" src="https://secure.img1-fg.wfcdn.com/im/55143880/resize-h445%5Ecompr-r85/1444/14441004/Menu+Wall+Mounted+Chalkboard.jpg" alt=""/>
-          <div>MARGARITA</div>
-          <br/>
-          <div>OLD FASHIONED</div>
-          <br/>
-          <div>MAI TAI</div>
-          <br/>
-          <div>MOJITO (please don't order)</div>
-          <br/>
-          <div>LEMON BASIL MARTINI</div>
-
+      <img className="background" src="https://github.com/swilcox21/drink-menu-template/blob/master/src/images/chalkboard.png?raw=true" alt=""/>
+      <div className="drinkList">
+            <div className="drink">MARGARITA</div>
+            <br/>
+            <div className="drink">OLD FASHIONED</div>
+            <br/>
+            <div className="drink">MAI TAI</div>
+            <br/>
+            <div className="drink">MOJITO (please don't order)</div>
+            <br/>
+            <div className="drink">LEMON BASIL MARTINI</div>
       </div>
+    </div>
+  </>
   );
 }
 
